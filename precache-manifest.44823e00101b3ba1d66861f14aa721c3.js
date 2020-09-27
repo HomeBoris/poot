bc7699019728aa9b178933c7c32f3fb4b252adfa
HomeBoris <51114840+HomeBoris@users.noreply.github.com>
2020-09-27 03:03:56 +0300

    Updates

---
+++ precache-manifest.44823e00101b3ba1d66861f14aa721c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34696dcf0adb1bc1827923b0c89ae629",
+    "url": "/poot/index.html"
+  },
+  {
+    "revision": "2f439afcc75559681710",
+    "url": "/poot/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "35d34f99cadf13ba9af0",
+    "url": "/poot/static/css/main.29416738.chunk.css"
+  },
+  {
+    "revision": "2f439afcc75559681710",
+    "url": "/poot/static/js/2.e1238486.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/poot/static/js/2.e1238486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d34f99cadf13ba9af0",
+    "url": "/poot/static/js/main.d56c77c0.chunk.js"
+  },
+  {
+    "revision": "96ae4a6e84a8a4865ceb",
+    "url": "/poot/static/js/runtime-main.9673136c.js"
+  }
+]);
